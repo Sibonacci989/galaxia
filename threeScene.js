d457c240e6c90de35f99c75148ce41c8e7b97fe8
Sarah Baig <sarahbaig@SimeenMirzasAir.lan>
2025-10-15 19:43:21 -0500

    added background and basic star shape

--- threeScene.js
+++ threeScene.js
@@ -1,6 +1,22 @@
-const scene = new THREE.Scene(); //that creates a new scene in the three.js library
-scene.background = new THREE.Color(0x000000); //that makes it black. scene.background is a property of the scene 
-scene.fog = new THREE.Fog(0x000000, 1, 1000); // it adds fog to the scene. parameters: color, near, far
-const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000); //creates a camera. parameters: fov, aspect, near, far
-const renderer = new THREE.WebGLRenderer(); 
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x0a0a20);
+scene.fog = new THREE.Fog(0x0a0a20, 1, 1000);
+
+//set up camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
+);
+camera.position.z = 5;
+
+//set up the renderer!! draws everything to the screen
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementById('canvas'),
+    antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xf0f0f0);
+renderer.shadowMap.enabled = true;
 
