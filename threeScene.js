487d83c8130e8bdc0711f8aa32c11b08ac90f1ce
Sarah Baig <sarahbaig@SimeenMirzasAir.lan>
2025-10-15 20:26:03 -0500

    added the star shape

--- threeScene.js
+++ threeScene.js
@@ -13,10 +13,30 @@ camera.position.z = 5;
 
 //set up the renderer!! draws everything to the screen
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.getElementById('canvas'),
+    canvas: document.querySelector('#threeCanvas'),
     antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xf0f0f0);
-renderer.shadowMap.enabled = true;
+
+const Light = new THREE.AmbientLight(0xffffff,0,2);
+scene.add(Light);
+const starLight = new THREE.PointLight(0xffffaa, 1, 10);
+scene.add(starLight);
+
+const starGeometry = new THREE.SphereGeometry(1,64,64);
+const starMaterial = new THREE.MeshStandardMaterial({
+    color:0xffffaa,
+    emissive: 0xbe99c9,
+    metalness: 0.5,
+    roughness: 0.4
+});
+const star = new THREE.Mesh(starGeometry, starMaterial);
+scene.add(star);
+starLight.position.copy(star.position);
+
+const spotGeometry = new THREE.SphereGeometry(0.05, 8, 8);
+const spotMaterial = new THREE.MeshBasicMaterial({color:0xffffff});
+const spots = [];
+
+
 
